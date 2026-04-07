d33f2b558dc000c0ed1d3e740fb91236215bb27e
andrei7radu <74138663+andrei7radu@users.noreply.github.com>
2026-04-07 10:32:05 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-set -e
+NDK=$ANDROID_NDK_HOME
 
-mkdir -p out
-
-$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ \
+$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ \
   -shared -fPIC \
-  -o out/libpathfix.so \
-  zygisk/pathfix.cpp
-
-mkdir -p module/zygisk
-cp out/libpathfix.so module/zygisk/
-
-cp module.prop module/
-cp customize.sh module/
-
-cd module
-zip -r ../zygisk-pathfix.zip *
+  zygisk/pathfix.cpp \
+  -o libpathfix.so \
+  -ldl
