952f25680549d2d2f003d6ca35f8a94b0ccf6f5c
andrei7radu <74138663+andrei7radu@users.noreply.github.com>
2026-04-07 10:34:20 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 NDK=$ANDROID_NDK_HOME
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin
 
-$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ \
+TARGET=aarch64-linux-android
+API=24
+
+$TOOLCHAIN/${TARGET}${API}-clang++ \
   -shared -fPIC \
   zygisk/pathfix.cpp \
   -o libpathfix.so \
